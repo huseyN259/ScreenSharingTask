90d9f5e7bc223b484a8fb2b9f4caf7b795f7246a
Ibra_xj86 <Ibra_xj86@itstep.edu.az>
2023-03-17 15:54:28 +0400

    Finished

---
+++ ClientSide/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace ClientSide;
+
+public partial class MainWindow : Window
+{
+	UdpClient client;
+	IPEndPoint remoteEP;
+
+	List<byte> list = new List<byte>();
+
+
+	public MainWindow()
+	{
+		InitializeComponent();
+
+		client = new UdpClient();
+		remoteEP = new(IPAddress.Parse("127.0.0.1"), 45678);
+	}
+
+
+	private async void Button_Click(object sender, RoutedEventArgs e)
+	{
+		var size = ushort.MaxValue - 29;
+		byte[] buffer = new byte[size];
+
+		await client.SendAsync(buffer, buffer.Length, remoteEP);
+
+		int lengthSize = buffer.Length;
+
+		int length = 0;
+		while (true)
+		{
+			do
+			{
+				try
+				{
+					var receiveResult = await client.ReceiveAsync();
+					buffer = receiveResult.Buffer;
+					length = buffer.Length;
+					list.AddRange(buffer);
+				}
+				catch (System.Exception)
+				{
+					MessageBox.Show("Server is not running...", "", MessageBoxButton.OK, MessageBoxImage.Warning);
+				}
+
+			} while (length == lengthSize);
+
+			Image.Source = GetImage(list.ToArray());
+			list.Clear();
+		}
+	}
+
+
+	private static BitmapImage GetImage(byte[] imageInfo)
+	{
+		var image = new BitmapImage();
+
+		using (var memoryStream = new MemoryStream(imageInfo))
+		{
+			memoryStream.Position = 0;
+
+			image.BeginInit();
+			image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+			image.CacheOption = BitmapCacheOption.OnLoad;
+			image.StreamSource = memoryStream;
+			image.EndInit();
+		}
+
+		image.Freeze();
+		return image;
+	}
+}
---
+++ ServerSide/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Drawing;
+using System.Net.Sockets;
+
+
+var server = new UdpClient(45678);
+
+
+while (true)
+{
+	UdpReceiveResult result = await server.ReceiveAsync();
+
+
+	new Task(async () =>
+	{
+		var remoteEP = result.RemoteEndPoint;
+
+		while (true)
+		{
+			await Task.Delay(10);
+
+			var image = TakeScreenshot();
+			var bytes = ImageToByte(image);
+			var size = ushort.MaxValue - 29;
+			var chunk = bytes.Chunk(size);
+			foreach (var buffer in chunk)
+				await server.SendAsync(buffer, buffer.Length, remoteEP);
+		}
+
+	}).Start();
+}
+
+
+#region Methods
+Image TakeScreenshot()
+{
+	Bitmap bitmap = new Bitmap(1920, 1080);
+
+	Graphics graphics = Graphics.FromImage(bitmap);
+	graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+
+	return bitmap;
+}
+
+
+byte[] ImageToByte(Image image)
+{
+	using (var stream = new MemoryStream())
+	{
+		image.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+
+		return stream.ToArray();
+	}
+}
+#endregion
